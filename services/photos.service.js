2dca34a528a753b5963eb0f0189f4b83e0a01237
FranciscoJMorales <morales.francisco0206@gmail.com>
2024-05-31 12:14:55 -0600

    feat: enrich photo data

--- services/photos.service.js
+++ services/photos.service.js
@@ -1,9 +1,21 @@
 export const FindPhotos = async () => {
-    const response = await fetch('https://jsonplaceholder.typicode.com/photos');
-    return response.json();
+    return await fetch('https://jsonplaceholder.typicode.com/photos').then(res => res.json());
 };
 
 export const FindPhoto = async (id) => {
-    const response = await fetch(`https://jsonplaceholder.typicode.com/photos/${id}`);
-    return response.json();
+    const photo = await fetch(`https://jsonplaceholder.typicode.com/photos/${id}`).then(res => res.json());
+    photo.album = await FindAlbum(photo.albumId);
+    // Remove albumId property since it is not a part of the expected output
+    delete photo.albumId;
+    return photo;
 };
+
+const FindAlbum = async (id) => {
+    const album = await fetch(`https://jsonplaceholder.typicode.com/albums/${id}`).then(res => res.json());
+    album.user = await FindUser(album.userId);
+    // Remove userId property since it is not a part of the expected output
+    delete album.userId;
+    return album;
+}
+
+const FindUser = async (id) => await fetch(`https://jsonplaceholder.typicode.com/users/${id}`).then(res => res.json());
