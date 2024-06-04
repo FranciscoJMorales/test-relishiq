9a03edcd757edb62737c7a2ffbf2f57c658f8e63
FranciscoJMorales <morales.francisco0206@gmail.com>
2024-06-04 12:08:26 -0600

    fix: increase photos limit

--- routes/photos.route.js
+++ routes/photos.route.js
@@ -11,8 +11,8 @@ PhotosRouter.get(
         query('title').trim().toLowerCase(),
         query('album.title').trim().toLowerCase(),
         query('album.user.email').trim().toLowerCase(),
-        query('limit', 'Limit must be a positive number lesser or equal to 100').isInt({ min:1, max: 100 }).optional(),
-        query('offset', 'Offset must be a number greater or equal to 0').isInt({ min:0 }).optional(),
+        query('limit', 'Limit must be a number greater to 0 and lesser or equal to 5000').isInt({ min:1, max: 5000 }).optional(),
+        query('offset', 'Offset must be a number greater or equal to 0 and lesser to 5000').isInt({ min:0, max: 4999 }).optional(),
         ValidateRequest
     ],
     GetPhotos
