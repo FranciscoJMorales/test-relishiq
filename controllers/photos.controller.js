f02fa095e6b24e41a3b9d963f66d4946f8019da0
FranciscoJMorales <morales.francisco0206@gmail.com>
2024-06-04 11:54:35 -0600

    fix: rename query params

--- controllers/photos.controller.js
+++ controllers/photos.controller.js
@@ -1,6 +1,9 @@
 import { FindPhotosPopulated, FindPhotoPopulated } from "../services/photos.service.js";
 
 export const GetPhotos = async (req, res) => {
+    // Rename query params for easier destructuring
+    req.query.albumTitle = req.query['album.title'];
+    req.query.albumUserEmail = req.query['album.user.email'];
     const photos = await FindPhotosPopulated(req.query);
     res.status(200).json(photos);
 };
