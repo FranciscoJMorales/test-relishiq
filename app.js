2d9d77b36132d0d9200c58906d6ff0a1f4438f09
FranciscoJMorales <morales.francisco0206@gmail.com>
2024-06-04 11:59:32 -0600

    feat: add morgan logs

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 import express from 'express';
+import morgan from 'morgan';
 import { AppRouter } from './routes/routes.js';
 
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => res.status(200).send());
+app.use(morgan('dev'));
+
+// Live check
+app.get('/', (_req, res) => res.status(200).send());
 
 app.use('/externalapi', AppRouter);
 
