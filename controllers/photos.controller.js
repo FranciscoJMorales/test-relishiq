a3283f1016a2400bcf42ffe19770788ad902f026
FranciscoJMorales <morales.francisco0206@gmail.com>
2024-06-04 12:31:58 -0600

    chore: catch errors

--- controllers/photos.controller.js
+++ controllers/photos.controller.js
@@ -1,15 +1,31 @@
 import { FindPhotosPopulated, FindPhotoPopulated } from "../services/photos.service.js";
 
 export const GetPhotos = async (req, res) => {
-    // Rename query params for easier destructuring
-    req.query.albumTitle = req.query['album.title'];
-    req.query.albumUserEmail = req.query['album.user.email'];
-    const photos = await FindPhotosPopulated(req.query);
-    res.status(200).json(photos);
+    try {
+        // Rename query params for easier destructuring
+        req.query.albumTitle = req.query['album.title'];
+        req.query.albumUserEmail = req.query['album.user.email'];
+        const photos = await FindPhotosPopulated(req.query);
+        res.status(200).json(photos);
+    }
+    catch {
+        res.status(500).json({
+            statusCode: 500,
+            message: 'An unexpected error has occurred. Please try again later',
+        });
+    }
 };
 
 export const GetPhoto = async (req, res) => {
-    const { id } = req.params;
-    const photo = await FindPhotoPopulated(id);
-    res.status(200).json(photo);
+    try {
+        const { id } = req.params;
+        const photo = await FindPhotoPopulated(id);
+        res.status(200).json(photo);
+    }
+    catch {
+        res.status(500).json({
+            statusCode: 500,
+            message: 'An unexpected error has occurred. Please try again later',
+        });
+    }
 };
