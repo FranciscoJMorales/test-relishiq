f55e35038c6e3ab62d9491648aa8a484ea6aa0ed
FranciscoJMorales <morales.francisco0206@gmail.com>
2024-06-04 12:11:12 -0600

    chore: add cors

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
 import express from 'express';
 import morgan from 'morgan';
+import cors from 'cors';
 import { AppRouter } from './routes/routes.js';
 
 const app = express();
 const port = 3000;
 
+// Logs
 app.use(morgan('dev'));
+
+// CORS
+app.use(cors());
+app.options('*', cors());
 
 // Live check
 app.get('/', (_req, res) => res.status(200).send());
